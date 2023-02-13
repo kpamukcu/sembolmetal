0d0c1756ac8ff2c656d3fdd33afff43411a76429
kpamukcu <kapawebtasarim@gmail.com>
2023-02-13 10:57:07 +0300

    Ürün Sayfasına Ekleme Yapıldı

--- urunler.php
+++ urunler.php
@@ -13,8 +13,8 @@
 <!-- Banner Section End -->
 
 <!-- Categories Section Start -->
-<section id="categories">
-    <div class="container">
+<section id="categories" class="py-5">
+    <div class="container-fluid">
         <div class="row">
             <div class="col-md-4">
                 <div class="card">
@@ -28,14 +28,116 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-4"></div>
-            <div class="col-md-4"></div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-7">
+                                <h3>Kategori Adı</h3>
+                            </div>
+                            <div class="col-5"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-7">
+                                <h3>Kategori Adı</h3>
+                            </div>
+                            <div class="col-5"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </section>
 <!-- Categories Section End -->
 
+<!-- Products Section Start -->
+<section id="products" class="py-5">
+    <div class="container-fluid">
+        <div class="row mb-4">
+            <div class="col-md-3">
+                <div class="card">
+                    <a href=""><img src="img/demo1.webp" alt="" class="img-fluid"></a>
+                    <div class="card-footer">
+                        <h2><a href="">Ürün Adı</a></h2>
+                        <small><a href="">Kategori Adı</a></small>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+            <div class="card">
+                    <a href=""><img src="img/demo1.webp" alt="" class="img-fluid"></a>
+                    <div class="card-footer">
+                        <h2><a href="">Ürün Adı</a></h2>
+                        <small><a href="">Kategori Adı</a></small>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+            <div class="card">
+                    <a href=""><img src="img/demo1.webp" alt="" class="img-fluid"></a>
+                    <div class="card-footer">
+                        <h2><a href="">Ürün Adı</a></h2>
+                        <small><a href="">Kategori Adı</a></small>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+            <div class="card">
+                    <a href=""><img src="img/demo1.webp" alt="" class="img-fluid"></a>
+                    <div class="card-footer">
+                        <h2><a href="">Ürün Adı</a></h2>
+                        <small><a href="">Kategori Adı</a></small>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-3">
+                <div class="card">
+                    <a href=""><img src="img/demo1.webp" alt="" class="img-fluid"></a>
+                    <div class="card-footer">
+                        <h2><a href="">Ürün Adı</a></h2>
+                        <small><a href="">Kategori Adı</a></small>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+            <div class="card">
+                    <a href=""><img src="img/demo1.webp" alt="" class="img-fluid"></a>
+                    <div class="card-footer">
+                        <h2><a href="">Ürün Adı</a></h2>
+                        <small><a href="">Kategori Adı</a></small>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+            <div class="card">
+                    <a href=""><img src="img/demo1.webp" alt="" class="img-fluid"></a>
+                    <div class="card-footer">
+                        <h2><a href="">Ürün Adı</a></h2>
+                        <small><a href="">Kategori Adı</a></small>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+            <div class="card">
+                    <a href=""><img src="img/demo1.webp" alt="" class="img-fluid"></a>
+                    <div class="card-footer">
+                        <h2><a href="">Ürün Adı</a></h2>
+                        <small><a href="">Kategori Adı</a></small>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Products Section End -->
+
 <?php require_once('footer.php') ?>
-
-
-
