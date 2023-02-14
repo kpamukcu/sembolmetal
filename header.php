3939809bbbea5f47722ff2718dfdd3ac493f8396
kpamukcu <kapawebtasarim@gmail.com>
2023-02-14 11:48:17 +0300

    Fav icon Eklendi

--- header.php
+++ header.php
@@ -10,8 +10,9 @@
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
   <link rel="stylesheet" href="css/style.css">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui/dist/fancybox.css"
-/>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui/dist/fancybox.css"/>
+
+  <link rel="icon" href="img/sembol-metal-icon.ico" type="image/x-icon" />
 
   <title>Sembol Metal | </title>
 </head>
