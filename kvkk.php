a95a51fef6b9b1ff7572c54ffc854763f1567ea3
cananucagac <ucagacc@gmail.com>
2023-02-16 14:17:31 +0300

    kvkkk sayfası oluşturuldu

---
+++ kvkk.php
@@ -0,0 +1,106 @@
+<?php require_once('header.php') ?>
+
+<section id="kvkk" class="py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <strong>KVKK POLİTİKAMIZ</strong>
+                <p>
+                    “SEMBOL METAL SAN. TİC.LTD.ŞTİ” (veya “Şirket” olarak anılacaktır) olarak, web sitelerimiz (“Site”), uygulamalarımız ya da dijital ortamda sizlerin kullanımına sunduğumuz benzeri tüm çevrimiçi ya da çevrimdışı mecralarımızı (anılan tüm mecralar birlikte “Platform” olarak anılacaktır.) kullanımınız veya ziyaretiniz sırasında sizlerin deneyimini geliştirmek için çerezler, pikseller, GIFler vb. birtakım teknolojilerden (“çerezler”) faydalanmaktayız.
+
+                    Bu teknolojilerin kullanımı başta 6698 sayılı Kişisel Verilerin Korunması Kanunu (“KVK Kanunu”) olmak üzere tabi olduğumuz mevzuata uygun şekilde gerçekleştirilmektedir.
+
+                    İşbu Çerez Aydınlatma Metni’nin amacı, Platformların kullanımı esnasında kullanılmakta olan çerez ve piksel gibi kişisel verilerin toplanması yoluyla kişisel verilerin işlenmesi nedeniyle elde edilen kişisel verilerin işlenmesine ilişkin olarak sizlere bilgi vermektir. İşbu metinde sitemizde ve uygulamamızda hangi amaçlarla hangi tür çerezleri kullandığımızı ve bu çerezleri nasıl kontrol edebileceğinizi sizlere açıklamak istiyoruz.
+
+                    SEMBOL METAL SAN.TİC.LTD.ŞTİ. olarak sitemizde ve uygulamamızda kullandığımız çerezleri kullanmaktan vazgeçebilir, bunların türlerini veya fonksiyonlarını değiştirebilir veya sitemize ve uygulamamızda yeni çerezler ekleyebiliriz. Dolayısıyla işbu Çerez Aydınlatma Metni’nin hükümlerini dilediğimiz zaman değiştirme hakkını saklı tutarız. Güncel Çerez Aydınlatma Metni üzerinde gerçekleştirilmiş olan her türlü değişiklik sitede, uygulamada veya herhangi bir kamuya açık mecrada yayınlanmakla birlikte yürürlük kazanacaktır. Son güncelleme tarihini metnin sonunda bulabilirsiniz.
+
+                    Kişisel verilerinizin Şirketimiz tarafından işlenme amaçları konusunda detaylı bilgilere [https://www.leta.com.tr/kisisel-verilerin-korunmasi] yer alan SEMBOL METAL SAN.TİC.LTD.ŞTİ Kişisel Verilerin Korunması ve İşlenmesi Politikası’ndan ulaşabilirsiniz. </p>
+
+                <strong>Kişisel Veri Toplamanın Yöntemi ve Hukuki Sebebi</strong>
+                <p> Kişisel verileriniz, internet sitemizi ziyaretiniz kapsamında veya uygulamamızı kullanmanız dolayısıyla elektronik ortamda çerezler yoluyla Şirketimizin meşru menfaatine yönelik hukuki sebebe dayalı olarak toplanmaktadır. Hedefleme ve profilleme vasıtasıyla gerçekleştirilen pazarlama ve reklamcılık faaliyetleri ise yalnızca açık rızanız olması halinde gerçekleştirilmektedir. Toplanan kişisel verileriniz Kanun’un 5. ve 6. maddelerinde belirtilen kişisel veri işleme şartları ve amaçları kapsamında işbu Çerez Aydınlatma Metni’nde belirtilen amaçlarla da işlenebilmektedir.
+
+                    Kişisel Verilerin Kimlere ve Hangi Amaçla Aktarılabileceği
+                    SEMBOL METAL SAN.TİC.LTD.ŞTİ olarak, Çerez Aydınlatma Metni kapsamındaki kişisel verilerinizi yukarıda belirtilen amaçların gerçekleştirilebilmesi ile sınırlı olarak ve mevzuata uygun şekilde tedarikçilerimize, kanunen yetkili kamu kurumlarına ve özel kişilere paylaşabiliriz</p>
+
+                <strong>Hangi Çerezler Hangi Amaçlarla Kullanılmaktadır?</strong>
+                <p> Genel Olarak
+
+                    SEMBOL METAL SAN.TİC.LTD.ŞTİ. olarak sitemizde çeşitli amaçlarla çerezler kullanmakta ve bu çerezler vasıtasıyla kişisel verilerinizi işlemekteyiz. Bu amaçlar başlıca şunlardır:</p>
+
+                <li>Site’nin çalışması için gerekli temel fonksiyonları gerçekleştirmek. Örneğin, oturum açan üyelerin Site’de farklı sayfaları ziyaret ederken tekrar şifre girmelerine gerek kalmaması.</li>
+
+                <li> Site’yi analiz etmek ve Site’nin performansını arttırmak. Örneğin, Site’nin üzerinde çalıştığı farklı sunucuların entegrasyonu, Site’yi ziyaret edenlerin sayısının tespit edilmesi ve buna göre performans ayarlarının yapılması ya da ziyaretçilerin aradıklarını bulmalarının kolaylaştırılması.</li>
+
+                <li> Site’nin işlevselliğini arttırmak ve kullanım kolaylığı sağlamak. Örneğin, Site üzerinden üçüncü taraf sosyal medya mecralarına paylaşımda bulunmak, Site’yi ziyaret eden ziyaretçinin daha sonraki ziyaretinde kullanıcı adı bilgisinin ya da arama sorgularının hatırlanması.</li>
+
+                <li> Kişiselleştirme, hedefleme ve reklamcılık faaliyeti gerçekleştirmek. Örneğin, ziyaretçilerin görüntüledikleri sayfa ve ürünler üzerinden ziyaretçilerin ilgi alanlarıyla bağlantılı reklam gösterilmesi.</li>
+
+                <p> KVK Kanunu’nun 5. ve 8. maddeleri uyarınca ve/veya ilgili mevzuattaki istisnaların varlığı halinde kişisel verileriniz mevzuat uyarınca gerekli olan hallerde rızanız doğrultusunda, aksi hallerde rızanız alınmaksızın yukarıdaki amaçlar doğrultusunda işlenebilecektir.SEMBOL METAL SAN.TİC.LTD.ŞTİ olarak, aydınlatma metni kapsamındaki kişisel verilerinizi yukarıda belirtilen amaçların gerçekleştirilebilmesi ile sınırlı olarak ve mevzuata uygun şekilde Şirketimizin hizmetlerinden faydalandığı üçüncü kişilerle, bağlı ortaklıklarımızla, iş ortaklarımızla ve grup şirketlerimiz ile paylaşabiliriz. Verilerin aktarıldığı tarafların kişisel verilerinizi dünyanın her yerindeki sunucularında saklayabileceğini belirtmek isteriz.</p>
+
+                <strong>Sitemizde ve Uygulamamızda Kullanılan Çerezler</strong>
+                <p> Aşağıda sitemizde ve uygulamamızda kullandığımız farklı türdeki çerezleri bulabilirsiniz. Sitemizde ve uygulamamızda hem birinci parti çerezler (ziyaret ettiğiniz site tarafından yerleştirilen) hem de üçüncü parti çerezleri (ziyaret ettiğiniz site haricindeki sunucular tarafından yerleştirilen) kullanılmaktadır.</p>
+
+                <strong>Zorunlu Çerezler</strong>
+                <p> Belli çerezlerin kullanımı sitemizin ve uygulamamızın doğru biçimde çalışması için zorunludur. Örneğin sitemizde oturum açtığınızda devreye giren kimlik doğrulama çerezleri, sitemizde bir sayfadan diğerine geçişinizde etkin olan oturumunuzun devam etmesini sağlamaktadır.</p>
+
+                <strong>İşlevsellik ve Tercih Çerezleri</strong>
+                <p> Bu çerezler sizlerin site ve uygulama üzerindeki tercihlerini ve seçimlerinizi hatırlayarak sitemizde sunulan hizmetlerin sizin için kişiselleşmesini sağlamaktadır. Örneğin sitemiz üzerindeki dil seçiminizi veya bir metin okurken seçmiş olduğunuz font boyutunu hatırlamamızı sağlar.</p>
+
+                <strong>Sosyal Medya Çerezleri</strong>
+                <p> Bu çerezler sizlerin sosyal medya kullanımlarınız hakkında bilgilerin toplanmasını sağlar. Örneğin Kişiselleştirilmiş reklamlar oluşturulması ya da market araştırmaları yapılması için Facebook/Twitter hesaplarınıza ait bilgilerin kullanılması için çerezler kullanılabilir.</p>
+
+                <strong>Performans ve Analiz Çerezleri</strong>
+                <p> Bu çerezler sayesinde sitemizi ve uygulamamızı kullanımınızı ve performans analizi yaparak sizlere verdiğimiz hizmetleri daha iyi hale getirebiliyoruz. Örneğin bu çerezler sayesinde ziyaretçilerimizin en çok hangi sayfaları görüntülediğini, sitemizin gerektiği gibi çalışıp çalışmadığını ve olası problemleri tespit edebiliyoruz.</p>
+
+                <strong>Hedefleme veya reklam çerezleri</strong>
+                <p> Sizlere sitemizde veya sitemiz haricindeki mecralarda ürün ve hizmet tanıtımını yapmak için çerezler kullanıyoruz. Ayrıca bazı iş ortaklarımızla sizlere sitemiz dahilin de veya dışında reklam ve tanıtım yapmak için iş birliğine gidebiliriz. Örneğin, sitemizde gördüğünüz bir reklama tıklayıp tıklamadığınızı, eğer reklam ilginizi çektikten sonra o reklam yönlendirdiği sitedeki hizmetten faydalanıp faydalanmadığınızı takip etmek için çerezler kullanılabilmektedir.</p>
+
+                <strong>Çerezlerin Kullanımını Nasıl Kontrol Edebilirim?</strong>
+                <p> Çerez ve benzeri teknolojilerin kullanımı konusunda ziyaretçi ve kullanıcılarımızın tercihleri bizler için esastır. Buna karşın, Platform’un çalışması için zorunlu olan Çerezler’in kullanılması gerekmektedir. Ek olarak bazı çerezlerin kapatılması halinde Platform’un birtakım işlevlerinin kısmen ya da tamamen çalışmayabileceğini hatırlatmak isteriz.
+
+                    Platform’da kullanılan çerezlere dair tercihlerinizi ne şekilde yönetebileceğinize ilişkin bilgiler aşağıdaki gibidir:
+
+                    Ziyaretçiler, Platform’u görüntüledikleri tarayıcı ayarlarını değiştirerek çerezlere ilişkin tercihlerini kişiselleştirme imkanına sahiptir. Eğer kullanılmakta olan tarayıcı bu imkânı sunmaktaysa, tarayıcı ayarları üzerinden çerezlere ilişkin tercihleri değiştirmek mümkündür. Böylelikle, tarayıcının sunmuş olduğu imkânlara göre farklılık gösterebilmekle birlikte, veri sahiplerinin çerezlerin kullanılmasını engelleme, çerez kullanılmadan önce uyarı almayı tercih etme veya sadece bazı çerezleri devre bırakma ya da silme imkanları bulunmaktadır.
+                    Bu konudaki tercihler kullanılan tarayıcıya göre değişiklik göstermekle birlikte genel açıklamaya <a href="https://www.aboutcookies.org/">https://www.aboutcookies.org/</a> adresinden ulaşmak mümkündür. Çerezlere ilişkin tercihlerin, ziyaretçinin Platform’a erişim sağladığı her bir cihaz özelinde ayrı ayrı yapılması gerekebilecektir.
+                    Google Analytics tarafından yönetilen çerezleri kapatmak için <a href="https://tools.google.com/dlpage/gaoptout">tıklayınız.</a> 
+                    Google tarafından sağlanan kişiselleştirilmiş reklam deneyimini yönetmek için <a href="https://support.google.com/adspolicy/answer/143465?hl=tr">tıklayınız.</a> 
+                    Birçok firmanın reklam faaliyetleri için kullandığı çerezler bakımından tercihler Your Online Choices üzerinden yönetilebilir.
+                    Mobil cihazlar üzerinden çerezleri yönetmek için mobil cihaza ait ayarlar menüsü kullanılabilir.
+                    Tarayıcınızın ayarlarını değiştirerek çerezlere ilişkin tercihlerinizi kişiselleştirme imkanına sahipsiniz. <br>
+                    Adobe Analytics <a href="http://www.adobe.com/uk/privacy/opt-out.html">http://www.adobe.com/uk/privacy/opt-out.html</a><br>
+
+                    AOL <a href=" https://help.aol.com/articles/restore-security-settings-and-enable-cookie-settings-on-browser"> https://help.aol.com/articles/restore-security-settings-and-enable-cookie-settings-on-browser</a><br>
+
+                    Google Adwords <a href=" https://support.google.com/ads/answer/2662922?hl=en"> https://support.google.com/ads/answer/2662922?hl=en</a><br>
+
+                    Google Analytics <a href="https://tools.google.com/dlpage/gaoptout">https://tools.google.com/dlpage/gaoptout</a> <br>
+
+                    Google Chrome <a href="http://www.google.com/support/chrome/bin/answer.py?hl=en&answer=95647">http://www.google.com/support/chrome/bin/answer.py?hl=en&answer=95647</a><br>
+
+                    Internet Explorer <a href="https://support.microsoft.com/en-us/help/17442/windows-internet-explorer-delete-manage-cookies">https://support.microsoft.com/en-us/help/17442/windows-internet-explorer-delete-manage-cookies</a> <br>
+
+                    Mozilla Firefox <a href="http://support.mozilla.com/en-US/kb/Cookies">http://support.mozilla.com/en-US/kb/Cookies</a><br>
+
+                    Opera <a href="http://www.opera.com/browser/tutorials/security/privacy/">http://www.opera.com/browser/tutorials/security/privacy/</a><br>
+
+                    Safari: <a href="https://support.apple.com/kb/ph19214?locale=tr_TR">https://support.apple.com/kb/ph19214?locale=tr_TR</a></p>
+
+                <strong>Veri Sahibi Olarak Haklarınız Nelerdir?</strong>
+                <p> KVK Kanunu’nun 11. maddesi uyarınca veri sahipleri,</p>
+
+                <li> Kişisel veri işlenip işlenmediğini öğrenme,</li>
+                <li> Kişisel verileri işlenmişse buna ilişkin bilgi talep etme,</li>
+                <li> Kişisel verilerin işlenme amacını ve bunların amacına uygun kullanılıp kullanılmadığını öğrenme,</li>
+                <li> Yurt içinde veya yurt dışında kişisel verilerin aktarıldığı üçüncü kişileri bilme,</li>
+                <li> Kişisel verilerin eksik veya yanlış işlenmiş olması hâlinde bunların düzeltilmesini isteme ve bu kapsamda yapılan işlemin kişisel verilerin aktarıldığı üçüncü kişilere bildirilmesini isteme,</li>
+                <li> KVK Kanunu ve ilgili diğer kanun hükümlerine uygun olarak işlenmiş olmasına rağmen, işlenmesini gerektiren sebeplerin ortadan kalkması hâlinde kişisel verilerin silinmesini veya yok edilmesini isteme ve bu kapsamda yapılan işlemin kişisel verilerin aktarıldığı üçüncü kişilere bildirilmesini isteme,</li>
+                <li> İşlenen verilerin münhasıran otomatik sistemler vasıtasıyla analiz edilmesi suretiyle kişinin kendisi aleyhine bir sonucun ortaya çıkmasına itiraz etme,</li>
+                <li> Kişisel verilerin kanuna aykırı olarak işlenmesi sebebiyle zarara uğraması hâlinde zararın giderilmesini talep etme
+                haklarına sahiptir.</li>
+                <li> Söz konusu haklarınıza ilişkin taleplerinizi, Başvuru Formu adresinde yer alan Veri Sahibi Başvuru Formu’nu doldurarak Şirketimize iletebilirsiniz. Talebinizin niteliğine göre en kısa sürede ve en geç otuz gün içinde başvurularınız ücretsiz olarak sonuçlandırılacaktır; ancak işlemin ayrıca bir maliyet gerektirmesi halinde Kişisel Verileri Koruma Kurulu tarafından belirlenecek tarifeye göre tarafınızdan ücret talep edilebilecektir.</li>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php require_once('footer.php') ?>
