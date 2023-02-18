b9e9a0fa277dc954060403919986098d1efcdaea
cananucagac <ucagacc@gmail.com>
2023-02-18 22:13:52 +0300

    blog sayfası makale olarak değiştirildi

---
+++ makale.php
@@ -0,0 +1,117 @@
+<?php require_once('header.php') ?>
+
+<!-- Blog Banner Section Start -->
+<section id="blogBanner" class="py-11" style="background-color: #e7e1d8;">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h1 class="display-4">Blog</h1>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Blog Banner Section End -->
+<!-- Blog Section Start -->
+<section id="blog" class="py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-9">
+                <img src="img/blog-photos1-815x437.webp" alt="">
+                <h2 class="pt-5 text-center">İç Mekanda Koltuk Seçerken Nelere Dikkat Edilmeli </h2>
+                <hr>
+                <small>Yayın Tarihi:14/11/2022</small><br><br>
+                <p class="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab distinctio ratione mollitia repellendus doloribus commodi! Facilis aliquid labore hic praesentium laudantium incidunt. Veritatis itaque laboriosam earum voluptas, perspiciatis sunt voluptatem nulla cum ullam distinctio commodi numquam nisi, provident sequi assumenda recusandae officiis suscipit possimus, similique eligendi? Eaque eveniet tempore obcaecati. <br> Accusantium cum nihil id exercitationem eligendi ratione facere eveniet deserunt ipsum illum voluptate amet beatae eum nesciunt doloribus officia, incidunt est culpa perferendis eius voluptas magnam iste molestiae? Eligendi quidem eius sit ullam libero consectetur incidunt et omnis numquam facere ad repellendus repudiandae maxime minus, suscipit eaque perferendis, ab voluptas, quibusdam nobis sint nesciunt. Error aliquid eum voluptates optio delectus reprehenderit iure ipsum eos vel, minus unde est corporis ratione inventore, architecto nesciunt officia esse cupiditate tempora, doloremque velit ullam temporibus! Explicabo perferendis tenetur necessitatibus?<br>
+                    Error aliquid eum voluptates optio delectus reprehenderit iure ipsum eos vel, minus unde est corporis ratione inventore, architecto nesciunt officia esse cupiditate tempora, doloremque velit ullam temporibus! Explicabo perferendis tenetur necessitatibus?
+                    Error aliquid eum voluptates optio delectus reprehenderit iure ipsum eos vel, minus unde est corporis ratione inventore, architecto nesciunt officia esse cupiditate tempora, doloremque velit ullam temporibus! Explicabo perferendis tenetur necessitatibus?
+                </p>
+            </div>
+            <div class="col-md-3 text-left">
+                <img src="img/blog-photos2-245x177.webp" alt="">
+                <h5 class="pt-5">Şıklık ve Konfor</h5>
+                <hr>
+                <p class="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, eveniet? Velit illum minima ex consequatur.</p>
+                <h5 class="pt-5">Popüler Yazılar</h5>
+                <hr>
+                <a href="">
+                    <div class="card mb-3" style="max-width: 540px;">
+                        <div class="row no-gutters">
+                            <div class="col-md-4">
+                                <img src="img/dresuar-100x100.webp" alt="...">
+                            </div>
+                            <div class="col-md-8">
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+                <a href="">
+                    <div class="card mb-3" style="max-width: 540px;">
+                        <div class="row no-gutters">
+                            <div class="col-md-4">
+                                <img src="img/sehpe-100x100.webp" alt="...">
+                            </div>
+                            <div class="col-md-8">
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+                <h5>Kategoriler</h5>
+                <hr>
+                <a href="">Dresuar</a><br>
+                <a href="">Sandalyeler</a><br>
+                <a href="">Koltuklar</a><br>
+                <a href="">Büfe ve Kitaplık</a><br>
+                <a href="">Sehpalar</a><br>
+                <a href="">Masalar</a><br>
+                <a href="">Bar Sandalyeleri</a>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Blog Section End -->
+<!-- Yorum Section Start -->
+<section id="yorum" class="py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h4 class="display-5 text-center">3 Yorum </h4><hr style="width: 25%;">
+                <br>
+                <div class="media">
+                    <img src="img/kisi1-64x64.webp" class="mr-3" alt="...">
+                    <div class="media-body">
+                        <h5 class="mt-0">Media heading</h5>
+                        <p>Standing on the frontline when the bombs start to fall. Heaven is jealous of our love, angels are crying from up above. Can't replace you with a million rings. Boy, when you're with me I'll give you a taste. There’s no going back. Before you met me I was alright but things were kinda heavy. Heavy is the head that wears the crown.</p>
+
+                        <div class="media mt-3">
+                            <a class="mr-3" href="#">
+                                <img src="img/kisi2-64x64.webp" alt="...">
+                            </a>
+                            <div class="media-body">
+                                <h5 class="mt-0">Media heading</h5>
+                                <p>Greetings loved ones let's take a journey. Yes, we make angels cry, raining down on earth from up above. Give you something good to celebrate. I used to bite my tongue and hold my breath. I'm ma get your heart racing in my skin-tight jeans. As I march alone to a different beat. Summer after high school when we first met. You're so hypnotizing, could you be the devil? Could you be an angel? It's time to bring out the big balloons. Thought that I was the exception. Bikinis, zucchinis, Martinis, no weenies.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="media">
+                    <img src="img/kisi3-64x64.webp" class="mr-3" alt="...">
+                    <div class="media-body">
+                        <h5 class="mt-0">Media heading</h5>
+                        <p>Will you do the same for me? It's time to face the music I'm no longer your muse. Heard it's beautiful, be the judge and my girls gonna take a vote. I can feel a phoenix inside of me. Heaven is jealous of our love, angels are crying from up above. Yeah, you take me to utopia.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Yorum Section End -->
+
+
+<?php require_once('footer.php') ?>
