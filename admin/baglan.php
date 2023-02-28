fcd1aea72a2d40901387c9a4b8690329e4de74c7
kpamukcu <kapawebtasarim@gmail.com>
2023-02-28 17:03:13 +0300

    Admin Paneline Başlandı

---
+++ admin/baglan.php
@@ -0,0 +1,3 @@
+<?php
+$db = new PDO('mysql:host=localhost;dbname=sembolmetal;charset=UTF8','root','')
+?>
---
+++ admin/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui/dist/fancybox.css" />
+
+    <link rel="icon" href="img/sembol-metal-icon.ico" type="image/x-icon" />
+
+
+    <title>Sembol Metal | Admin Paneli</title>
+</head>
+
+<body>
+
+    <!-- Admin Panel Login Start -->
+    <section id="login">
+        <div class="container">
+            <div class="row">
+                <div class="col-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <img src="../img/sembol-metal-logo-siyah-156x105.webp" alt="">
+                            <form method="post">
+                                <div class="form-group">
+                                    <input type="text" name="kadi" placeholder="Kullanıcı Adınız" class="form-control">
+                                </div>
+                                <div class="form-group">
+                                    <input type="password" name="sifre" placeholder="Şifreniz" class="form-control">
+                                </div>
+                                <div class="form-group">
+                                    <input type="submit" value="Giriş Yap" class="btn btn-success w-100">
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Admin Panel Login End -->
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.umd.js"></script>
+
+</body>
+
+</html>
