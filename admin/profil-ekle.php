ce004cd40c2837ed33661fb1410946772ab2d773
kpamukcu <kapawebtasarim@gmail.com>
2023-06-08 13:22:27 +0300

    Profil Ekleme Modülü Düzenlendi

--- admin/profil-ekle.php
+++ admin/profil-ekle.php
@@ -100,37 +100,174 @@ if (isset($_GET['id'])) {
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <table class="table table-striped">
-                    <thead>
-                        <tr>
-                            <th>Ebat</th>
-                            <th class="text-center">600 cm Fiyatı</th>
-                            <th class="text-center">Türü</th>
-                            <th class="text-center">Düzenle</th>
-                            <th class="text-center">Sil</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                        $profilList = $db->prepare('select * from profilekle order by ebat');
-                        $profilList->execute();
 
-                        if ($profilList->rowCount()) {
-                            foreach ($profilList as $profilListSatir) {
-                        ?>
+
+
+                <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link active" id="pills-aluminyum-tab" data-toggle="pill" data-target="#pills-aluminyum" type="button" role="tab" aria-controls="pills-aluminyum" aria-selected="true">Alüminyum</button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link" id="pills-demir-tab" data-toggle="pill" data-target="#pills-demir" type="button" role="tab" aria-controls="pills-demir" aria-selected="false">Demir</button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link" id="pills-paslanmaz-tab" data-toggle="pill" data-target="#pills-paslanmaz" type="button" role="tab" aria-controls="pills-paslanmaz" aria-selected="false">Paslanmaz</button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link" id="pills-pirinc-tab" data-toggle="pill" data-target="#pills-pirinc" type="button" role="tab" aria-controls="pills-pirinc" aria-selected="false">Prinç</button>
+                    </li>
+                </ul>
+                <div class="tab-content" id="pills-tabContent">
+                    <div class="tab-pane fade show active" id="pills-aluminyum" role="tabpanel" aria-labelledby="pills-aluminyum-tab">
+
+                        <table class="table table-striped">
+                            <thead>
                                 <tr>
-                                    <td><?php echo $profilListSatir['ebat']; ?></td>
-                                    <td class="text-center"><?php echo $profilListSatir['fiyat6m']; ?>₺</td>
-                                    <td class="text-center"><?php echo $profilListSatir['malTuru']; ?></td>
-                                    <td class="text-center"><a href="profil-ekle.php?ebat=<?php echo $profilListSatir['ebat']; ?>" class="btn btn-warning">Düzenle</a></td>
-                                    <td class="text-center"><a href="profil-ekle.php?id=<?php echo $profilListSatir['id']; ?>" class="btn btn-danger">Sil</a></td>
+                                    <th>Ebat</th>
+                                    <th class="text-center">600 cm Fiyatı</th>
+                                    <th class="text-center">Türü</th>
+                                    <th class="text-center">Düzenle</th>
+                                    <th class="text-center">Sil</th>
                                 </tr>
-                        <?php
-                            }
-                        }
-                        ?>
-                    </tbody>
-                </table>
+                            </thead>
+                            <tbody>
+                                <?php
+                                $profilList = $db->prepare('select * from profilekle where malTuru="Alüminyum" order by ebat');
+                                $profilList->execute();
+
+                                if ($profilList->rowCount()) {
+                                    foreach ($profilList as $profilListSatir) {
+                                ?>
+                                        <tr>
+                                            <td><?php echo $profilListSatir['ebat']; ?></td>
+                                            <td class="text-center"><?php echo $profilListSatir['fiyat6m']; ?>₺</td>
+                                            <td class="text-center"><?php echo $profilListSatir['malTuru']; ?></td>
+                                            <td class="text-center"><a href="profil-ekle.php?ebat=<?php echo $profilListSatir['ebat']; ?>" class="btn btn-warning">Düzenle</a></td>
+                                            <td class="text-center"><a href="profil-ekle.php?id=<?php echo $profilListSatir['id']; ?>" class="btn btn-danger">Sil</a></td>
+                                        </tr>
+                                <?php
+                                    }
+                                }
+                                ?>
+                            </tbody>
+                        </table>
+
+
+
+
+                    </div>
+                    <div class="tab-pane fade" id="pills-demir" role="tabpanel" aria-labelledby="pills-demir-tab">
+
+
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th>Ebat</th>
+                                    <th class="text-center">600 cm Fiyatı</th>
+                                    <th class="text-center">Türü</th>
+                                    <th class="text-center">Düzenle</th>
+                                    <th class="text-center">Sil</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                $profilList = $db->prepare('select * from profilekle where malTuru="Demir" order by ebat');
+                                $profilList->execute();
+
+                                if ($profilList->rowCount()) {
+                                    foreach ($profilList as $profilListSatir) {
+                                ?>
+                                        <tr>
+                                            <td><?php echo $profilListSatir['ebat']; ?></td>
+                                            <td class="text-center"><?php echo $profilListSatir['fiyat6m']; ?>₺</td>
+                                            <td class="text-center"><?php echo $profilListSatir['malTuru']; ?></td>
+                                            <td class="text-center"><a href="profil-ekle.php?ebat=<?php echo $profilListSatir['ebat']; ?>" class="btn btn-warning">Düzenle</a></td>
+                                            <td class="text-center"><a href="profil-ekle.php?id=<?php echo $profilListSatir['id']; ?>" class="btn btn-danger">Sil</a></td>
+                                        </tr>
+                                <?php
+                                    }
+                                }
+                                ?>
+                            </tbody>
+                        </table>
+
+
+                    </div>
+                    <div class="tab-pane fade" id="pills-paslanmaz" role="tabpanel" aria-labelledby="pills-paslanmaz-tab">
+
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th>Ebat</th>
+                                    <th class="text-center">600 cm Fiyatı</th>
+                                    <th class="text-center">Türü</th>
+                                    <th class="text-center">Düzenle</th>
+                                    <th class="text-center">Sil</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                $profilList = $db->prepare('select * from profilekle where malTuru="Paslanmaz" order by ebat');
+                                $profilList->execute();
+
+                                if ($profilList->rowCount()) {
+                                    foreach ($profilList as $profilListSatir) {
+                                ?>
+                                        <tr>
+                                            <td><?php echo $profilListSatir['ebat']; ?></td>
+                                            <td class="text-center"><?php echo $profilListSatir['fiyat6m']; ?>₺</td>
+                                            <td class="text-center"><?php echo $profilListSatir['malTuru']; ?></td>
+                                            <td class="text-center"><a href="profil-ekle.php?ebat=<?php echo $profilListSatir['ebat']; ?>" class="btn btn-warning">Düzenle</a></td>
+                                            <td class="text-center"><a href="profil-ekle.php?id=<?php echo $profilListSatir['id']; ?>" class="btn btn-danger">Sil</a></td>
+                                        </tr>
+                                <?php
+                                    }
+                                }
+                                ?>
+                            </tbody>
+                        </table>
+
+                    </div>
+                    <div class="tab-pane fade" id="pills-pirinc" role="tabpanel" aria-labelledby="pills-pirinc-tab">
+
+
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th>Ebat</th>
+                                    <th class="text-center">600 cm Fiyatı</th>
+                                    <th class="text-center">Türü</th>
+                                    <th class="text-center">Düzenle</th>
+                                    <th class="text-center">Sil</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                $profilList = $db->prepare('select * from profilekle where malTuru="Pirinç" order by ebat');
+                                $profilList->execute();
+
+                                if ($profilList->rowCount()) {
+                                    foreach ($profilList as $profilListSatir) {
+                                ?>
+                                        <tr>
+                                            <td><?php echo $profilListSatir['ebat']; ?></td>
+                                            <td class="text-center"><?php echo $profilListSatir['fiyat6m']; ?>₺</td>
+                                            <td class="text-center"><?php echo $profilListSatir['malTuru']; ?></td>
+                                            <td class="text-center"><a href="profil-ekle.php?ebat=<?php echo $profilListSatir['ebat']; ?>" class="btn btn-warning">Düzenle</a></td>
+                                            <td class="text-center"><a href="profil-ekle.php?id=<?php echo $profilListSatir['id']; ?>" class="btn btn-danger">Sil</a></td>
+                                        </tr>
+                                <?php
+                                    }
+                                }
+                                ?>
+                            </tbody>
+                        </table>
+
+
+                    </div>
+                </div>
+
+
             </div>
         </div>
     </div>
